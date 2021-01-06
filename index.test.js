1667700eecf7f69ea1df487970e1a4e78d4bf561
Joshua Campbell <joshuacampbell@Joshua-Campbell.local>
2021-01-06 11:39:03 -0600

    Fix tests and and .env.example

--- index.test.js
+++ index.test.js
@@ -9,38 +9,34 @@ let productId;
 describe("client", () => {
     test("should return an array from 'GET v3/customers'", async () => {
         const customers = await bc.get('v3/customers');
-        return (
-            expect(customers).toHaveProperty('length') &&
-            expect(customers.length).toBeGreaterThan(0)
-        );
+            expect(customers).toHaveProperty('length');
+            expect(customers.length).toBeGreaterThan(0);
     })
 
     test("should return a product from 'POST v3/catalog/products'", async () => {
         const product = await bc.post(`v3/catalog/products`, {name: productName, type:'physical', weight:1, price:99.99})
         productId = product.id;
-        return(
-            expect(product).toHaveProperty("name") &&
-            expect(product.name).toBe(productName) &&
-            expect(product).toHaveProperty("type") &&
-            expect(product.name).toBe("physical") &&
-            expect(product).toHaveProperty("weight") &&
-            expect(product.name).toBe(1) &&
-            expect(product).toHaveProperty("price") &&
-            expect(product.name).toBe(99.99)
-        )
+
+        expect(product).toHaveProperty("name");
+        expect(product.name).toBe(productName);
+        expect(product).toHaveProperty("type");
+        expect(product.type).toBe("physical");
+        expect(product).toHaveProperty("weight");
+        expect(product.weight).toBe(1);
+        expect(product).toHaveProperty("price");
+        expect(product.price).toBe(99.99);
     })
 
     test("should return an updated product from 'PUT v3/catalog/products'", async () => {
         const product = await await bc.put(`v3/catalog/products/${productId}`, {price:909.99})
-        return(
-            expect(product).toHaveProperty("name") &&
-            expect(product.name).toBe(productName) &&
-            expect(product).toHaveProperty("type") &&
-            expect(product.name).toBe("physical") &&
-            expect(product).toHaveProperty("weight") &&
-            expect(product.name).toBe(1) &&
-            expect(product).toHaveProperty("price") &&
-            expect(product.name).toBe(909.99)
-        )
+
+        expect(product).toHaveProperty("name");
+        expect(product.name).toBe(productName);
+        expect(product).toHaveProperty("type");
+        expect(product.type).toBe("physical");
+        expect(product).toHaveProperty("weight");
+        expect(product.weight).toBe(1);
+        expect(product).toHaveProperty("price");
+        expect(product.price).toBe(909.99);
     })
 })

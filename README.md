ad03cc1450f446372597733f6a6bc84067f38d9e
Hunter Shaw <hunter.shaw@classyllama.com>
2021-04-08 16:17:24 -0500

    New paginated functions

--- README.md
+++ README.md
@@ -1,32 +1,78 @@
 # bigcommerce-client
 Simple API client for the BigCommerce Management API
 
-## Usage
+## Installation
 
 `npm i bigcommerce-client`
 
+## Usage
+
 ```javascript
-const client = require('bigcommerce-client')
-const bc = new client('storeHash', 'authToken')
+const bc_client = require('bigcommerce-client')
+const store = new client('storeHash', 'authToken')
+```
 
-;(async _=>{
-    const customers = await bc.get('v3/customers')
-    console.log('customers:', customers, '\n')
-  
-    const physicalProducts = await bc.get('v3/catalog/products', {type:'physical'})
-    console.log('physicalProducts', physicalProducts, '\n')
+### Get a Resource
 
+```javascript
+async function getFromSKU(sku){
+    return await store.get('v3/catalog/products', {sku})
+}
+```
+
+### Get All Resources
+
+```javascript
+async function getOutOfStock(sku){
+    return await store.getAll('v3/catalog/products', {inventory_level:0})
+}
+```
+
+### Paginate All Resources
+
+```javascript
+async function updateOutOfStock(sku){
+    await store.paginate('v3/catalog/products', (products)=>{
+        await store.put('v3/catalog/products', products.map((product)=>{
+            product.inventory_level = 1
+            return product
+        }))
+    }, {limit:10, inventory_level:0, include_fields:'inventory_level'})
+}
+```
+
+### Create a Resource
+
+```javascript
+async function createDigitalProduct(name, price){
+    return (await store.post('v3/catalog/products', {name, price, type:'digital', weight:0})).id
+}
+```
+
+### Update Resource
+
+```javascript
+async function changePrice(id, price){
     try {
-        let newProduct = await bc.post('v3/catalog/products', {name:'New Product', type:'physical', weight:1, price:99.99})
-        
-        console.log('newProduct', newProduct, '\n')
+        await store.put(`v3/catalog/products/${id}`, {price}))
+    } catch (new Error('ID not found'))
+}
+```
 
-        let updatedProduct = await bc.put(`v3/catalog/products/${newProduct.id}`, {price:109.99})
+### Delete a Resource
 
-        console.log('updatedProduct', updatedProduct, '\n')
-    }
-    catch(e) { console.log(e.message) }
+```javascript
+async function deleteProduct(id){
+    try {
+        await store.delete(`v3/catalog/products/${id}`))
+    } catch (new Error('ID not found'))
+}
+```
 
-})();
+### Delete All Resources
 
+```javascript
+async function deleteAllProducts(){
+    await store.deleteAll(`v3/catalog/products`))
+}
 ```
--- index.js
+++ index.js
@@ -6,8 +6,9 @@ module.exports = class {
      * Construct BigCommerceClient Instance.
      * @param {string} hash Store Hash (ex: gha3w9n1at)
      * @param {string} token API Token (ex: j3aovsvag3vg88hhyl4qt89q6ag2b6b)
+     * @param {boolean} debug Enable console output for every request
      */
-    constructor(hash, token){
+    constructor(hash, token, debug=false){
         this.base = `https://api.bigcommerce.com/stores/${hash}/`
         this.headers = {
             'X-Auth-Token':token,
@@ -15,16 +16,7 @@ module.exports = class {
             'Content-Type':'application/json'
         }
         this.meta = {}
-    }
-
-    /**
-     * Builds a safe url from a query object.
-     * @param {object} queries Object w/ keys & string values of each url query parameter (example: {sku:'10205'})
-     */
-    buildQueries(queries){
-        let result = []
-        for (let query in queries) { if (queries[query] !== undefined) result.push(querystring.escape(query) + '=' + querystring.escape(queries[query])) }
-        return '?' + result.join('&')
+        this.debug = debug
     }
 
     /**
@@ -34,17 +26,56 @@ module.exports = class {
      * @param {string} endpoint Url endpoint from version onward (example: 'v3/catalog/products')
      * @param {object} queries Object w/ keys & string values of each url query parameter (example: {sku:'10205'})
      */
-    async get(endpoint, queries=null){
-        if (queries !== null) endpoint += this.buildQueries(queries)
+    async get(endpoint, queries={}){
+        endpoint += '?' + querystring.stringify(queries)
         let response
         try {
+            if (this.debug) console.log('GET', this.base + endpoint)
             response = await fetch(this.base + endpoint, {headers:this.headers, timeout:15000})
         } catch(e){return await this.get(endpoint, queries)}
         if (response.ok) return await this.readResponse(response)
         throw new Error(`${response.status} - ${response.statusText}: ${await response.text()}`)
     }
 
+    /**
+     * Function to perform on each page returned by endpoint. Should accept an array of objects from page.
+     * @callback eachPage
+     * @param {object[]} pageContents
+     */
 
+    /**
+     * Performs sequential GET requests to the BigCommerce Management API at the specified endpoint. For each page in query it will perform the provided callback, passing an array of objects in page.
+     * @async
+     * @returns {null}
+     * @param {string} endoint Url endpoint from version onward (example: 'v3/catalog/products')
+     * @param {eachPage} eachPage Callback for each page provided by endpoint
+     * @param {object} queries Object w/ keys & string values of each url query parameter (example: {sku:'10205'}). Page & limit can be passed to control start & page size.
+     */
+    async paginate(endpoint, eachPage, queries={}){
+        eachPage(await this.get(endpoint, queries))
+        if (this.debug) console.log('CURRENT PAGE:', this.meta.pagination.current_page, 'TOTAL PAGES:', this.meta.pagination.total_pages)
+        if (this.meta.pagination.current_page === this.meta.pagination.total_pages) return
+        for (let current_page = this.meta.pagination.current_page + 1; current_page <= this.meta.pagination.total_pages; current_page++){
+            queries.page = current_page
+            eachPage(await this.get(endpoint, queries))
+            if (this.debug) console.log('CURRENT PAGE:', current_page, 'TOTAL PAGES:', this.meta.pagination.total_pages)
+        }
+    }
+
+    /**
+     * Performs sequential GET request to the BigCommerce Management API at the specified endpoint. Concatenates results from all pages.
+     * @async
+     * @returns {object[]} Concatenated JSON data returned from a 2-- request
+     * @param {string} endpoint Url endpoint from version onward (example: 'v3/catalog/products')
+     * @param {object} queries Object w/ keys & string values of each url query parameter (example: {sku:'10205'})
+     */
+    async getAll(endpoint, queries={}){
+        let result = []
+        await this.paginate(endpoint, (items)=>{
+            result = result.concat(items)
+        }, queries)
+        return result
+    }
 
     /**
      * Performs a POST request to the BigCommerce Management API at the specified endpoint. Throws error w/ http status information if non-200 response is returned.
@@ -56,6 +87,7 @@ module.exports = class {
     async post(endpoint, body){
         let response
         try {
+            if (this.debug) console.log('POST', this.base + endpoint)
             response = await fetch(this.base + endpoint, {method:'post', headers:this.headers, timeout:15000, body:JSON.stringify(body)})
         } catch (e) {return await this.post(endpoint, body)}
         if (response.ok) return await this.readResponse(response)
@@ -72,6 +104,7 @@ module.exports = class {
     async put(endpoint, body){
         let response
         try {
+            if (this.debug) console.log('PUT', this.base + endpoint)
             response = await fetch(this.base + endpoint, {method:'put', headers:this.headers, timeout:15000, body:JSON.stringify(body)})
         } catch (e) {return await this.put(endpoint, body)}
         if (response.ok) return await this.readResponse(response)
@@ -84,14 +117,35 @@ module.exports = class {
      * @param {string} endpoint Url endpoint from version onward (example: 'v3/catalog/products')
      * @param {object} queries Object w/ keys & string values of each url query parameter (example: {sku:'10205'})
      */
-    async delete(endpoint, queries){
-        if (queries !== null) endpoint += this.buildQueries(queries)
+    async delete(endpoint, queries={}){
+        endpoint += '?' + querystring.stringify(queries)
         let response
         try {
+            if (this.debug) console.log('DELETE', this.base + endpoint)
             response = await fetch(this.base + endpoint, {method:'delete', headers:this.headers, timeout:15000})
         } catch(e){await this.delete(endpoint, queries)}
         if (response.ok) return
         throw new Error(`${response.status} - ${response.statusText}: ${await response.text()}`)
+    }
+
+    /**
+     * Performs sequential DELETE requests to the BigCommerce Management API at the specified endpoint. Will perform a getAll request, then for each ID returned, it will perform a DELETE.
+     * @async
+     * @param {string} endpoint Url endpoint from version onward (example: 'v3/catalog/products')
+     * @param {object} queries Object w/ keys & string values of each url query parameter (example: {sku:'10205'}). This function overwrites the limit propery to 3 to achieve high request concurrency
+     */
+    async deleteAll(endpoint, queries={}){
+        queries.limit = 3
+        let items = await this.get(endpoint, queries)
+        while (items.length){
+            console.log('Im about to delete this stuff', items.map((item)=>item.name))
+            await Promise.all([
+                items[0] !== undefined ? this.delete(endpoint + '/' + items[0].id) : undefined,
+                items[1] !== undefined ? this.delete(endpoint + '/' + items[1].id) : undefined,
+                items[2] !== undefined ? this.delete(endpoint + '/' + items[2].id) : undefined
+            ])
+            items = await this.get(endpoint, queries)
+        }
     }
 
     async readResponse(response){
